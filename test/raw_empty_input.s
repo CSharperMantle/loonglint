a30fa06af204b74d05b6cdb65d3115c4e1847f52
Rong Bao <rong.bao@csmantle.top>
2026-09-08 09:33:55 +0800

    MAR - Part 10: Recategorize LoongArch-specific tests into its own folder

--- test/raw_empty_input.s
+++ test/raw_empty_input.s
@@ -1,10 +1,7 @@
 ## Reject empty raw input.
 ## SPDX-License-Identifier: GPL-3.0-or-later
 
-# RUN: llvm-mc -triple=loongarch64 -filetype=obj %s -o %t.o
-# RUN: llvm-objcopy -O binary --only-section=.text %t.o %t.bin
+# RUN: touch %t.bin
 # RUN: not loonglint --input-format=raw --arch=loongarch64 %t.bin 2>&1 | FileCheck %s
 
 # CHECK: loonglint: error: input '{{.*}}.bin' is empty
-
-.text
