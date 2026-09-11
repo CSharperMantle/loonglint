CSharperMantle/loonglint

commit 76a7b76008af48b225990c7afc055fdf0661c779

Derive max section count in sm_jitdump2elf.py from SHN_LORESERVE

script/sm_jitdump2elf.py
@@ -10,7 +10,7 @@ from typing import Annotated, NamedTuple, final
 
 import typer
 from construct import Bytes, Int8ul, Int16ul, Int32ul, Int64ul, Struct
-from elftools.elf.constants import E_FLAGS, SH_FLAGS
+from elftools.elf.constants import E_FLAGS, SH_FLAGS, SHN_INDICES
 from elftools.elf.enums import (
     ENUM_ATTR_TAG_RISCV,
     ENUM_E_MACHINE,
@@ -584,7 +584,7 @@ def main(
         typer.Option(
             "--max-sections",
             min=1,
-            max=65534,
+            max=SHN_INDICES.SHN_LORESERVE - 16,  # Reserve enough space
             help="Split output into chunks of at most this many sections",
         ),
     ] = 65534,

commit b1bc9b0695f5dd350362ce8f2ebe3d5b90dc04ba

Fix bad default sections count in sm_jitdump2elf.py
Fixes: 76a7b76008af48b225990c7afc055fdf0661c779 ("Derive max section
 count in sm_jitdump2elf.py from SHN_LORESERVE")

script/sm_jitdump2elf.py
@@ -587,7 +587,7 @@ def main(
             max=SHN_INDICES.SHN_LORESERVE - 16,  # Reserve enough space
             help="Split output into chunks of at most this many sections",
         ),
-    ] = 65534,
+    ] = SHN_INDICES.SHN_LORESERVE - 16,
 ) -> None:
     data = dump.read_bytes()
     objects = parse_jitdump(data)